f1d5dfbcca3d13e45b13d3cc19454a14c2aa5480
Artem Hoshkoderia <goshkoderia@gmail.com>
2022-01-26 21:10:01 +0200

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryREf = document.querySelector(".gallery");
+
+const imagesMarkup = galleryItems.map(({preview,original,description})=>
+`<li class="gallery__item">
+<a class="gallery__link" href="#">
+<img class="gallery__image" src=${preview} alt="${description}" data-sourse="${original}" width="340" heigth="auto">
+</a>`).join("");
+galleryREf.insertAdjacentHTML("beforeend",imagesMarkup);
+
+
+
+const clickHandler = (event)=>{
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.sourse}" width="1280" height="auto">`)
+    instance.show();
+};
+
+
+galleryREf.addEventListener("click",clickHandler);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const imagesMarkup = galleryItems.map(({preview,original,description})=>
+`<li><a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}"/>
+</a></li>`).join("");
+
+galleryRef.insertAdjacentHTML("beforeend",imagesMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {  captionsData: "alt",
+captionDelay: 250,
+captionType: "attr",
+captionPosition:"bottom",});
