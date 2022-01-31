4735c0f6ab6ae7c72dd51f777d95c358b6ba022c
Artem Hoshkoderia <goshkoderia@gmail.com>
2022-01-31 09:49:58 +0200

    work on bags

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,15 +6,17 @@ const galleryREf = document.querySelector(".gallery");
 const imagesMarkup = galleryItems.map(({preview,original,description})=>
 `<li class="gallery__item">
 <a class="gallery__link" href="#">
-<img class="gallery__image" src=${preview} alt="${description}" data-sourse="${original}" width="340" heigth="auto">
+<img class="gallery__image" src=${preview} alt="${description}" data-sourse="${original}" width="340" height="auto">
 </a>`).join("");
 galleryREf.insertAdjacentHTML("beforeend",imagesMarkup);
+
+console.log(imagesMarkup);
 
 
 
 const clickHandler = (event)=>{
     event.preventDefault();
-    const instance = basicLightbox.create(`<img src="${event.target.dataset.sourse}" width="1280" height="auto">`)
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.sourse}" alt="${event.target.alt}" width="1280" height="auto">`)
     instance.show();
 };
 
